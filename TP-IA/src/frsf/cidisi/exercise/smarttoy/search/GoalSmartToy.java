fdfab00c789ebfaeb239b123f19b77bb6fef0fea
matiastibaldo <matias.tiba@gmail.com>
2018-05-10 13:40:01 -0300

    prueba de meta

--- TP-IA/src/frsf/cidisi/exercise/smarttoy/search/GoalSmartToy.java
+++ TP-IA/src/frsf/cidisi/exercise/smarttoy/search/GoalSmartToy.java
@@ -2,6 +2,7 @@
 
 package frsf.cidisi.exercise.smarttoy.search;
 
+import frsf.cidisi.faia.agent.Perception;
 import frsf.cidisi.faia.agent.search.GoalTest;
 import frsf.cidisi.faia.state.AgentState;
 
@@ -9,12 +10,27 @@ public class GoalSmartToy extends GoalTest {
 
     @Override
     public boolean isGoalState (AgentState agentState) {
+    	
+    	 AgentSmartToyState estadoAgente = (AgentSmartToyState) agentState;
+    	 int habitacionAgente= estadoAgente.getUbicacionInicial().getFirst().getIdHabitacion();
+    	 int habitacionSmartphone = estadoAgente.getUbicacionInicial().getFirst().getIdHabitacion();
+    	 int ubicacionAgente[] = estadoAgente.getUbicacionAgente().getSecond();
+    	 int ubicacionSmartphone[] = estadoAgente.getUbicacionAgente().getSecond(); 
     
+    	if(habitacionAgente == habitacionSmartphone & //habitacion smartphone misma que smartoy
+    			ubicacionAgente[0]==ubicacionSmartphone[0] & //ubicacion en la pieza es la misma
+    			ubicacionAgente[1]==ubicacionSmartphone[1]){
+    		return true;
+    	}
+    	else{
+    		return false;
+    	}
+    	/*
     	// TODO: Complete Method
         if  (true) //(normaVectorialIn(ubicacionAgente(),UbicacionSmartPhone()) == 1)
         	{
             return true;
         	}
-        return false;
+        return false;*/
 	}
 }
